9f0ff8ed45619d4e095b97a676e8aa6ca6420d78
Anas <anasosama17@gmail.com>
2018-01-03 14:07:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,11 @@
 #include <iostream>
-#include<vector>
+
 #include<algorithm>
-#include<bits/stdc++.h>
+
 #include<cstdlib>
 using namespace std;
 
-void sortStrings(char arr[][100], int n)
-{
-    char temp[100];
 
-    // Sorting strings using bubble sort
-    for (int j=0; j<n-1; j++)
-    {
-        for (int i=j+1; i<n; i++)
-        {
-            if (strcmp(arr[j], arr[i]) > 0)
-            {
-                strcpy(temp, arr[j]);
-                strcpy(arr[j], arr[i]);
-                strcpy(arr[i], temp);
-            }
-        }
-    }
-}
-
-class b
-{
-protected :
-    int v;
-public:
-    b()
-    {
-        v=9;
-    }
-    b( int c)
-    {
-        v=c;
-    }
-    void show ()
-    {
-
-        cout<<v<<endl;
-    }
-
-
-
-
-    b operator ++()
-    {
-
-        return b (++v);
-    }
-    b operator ++(int)
-    {
-
-        return b(v++);
-    }
-
-    b operator =(b c)
-    {
-        v=c.v;
-        return v;
-    }
-
-
-};
 int main()
 {    cout<<"\t\t\twellcome to  name sorting (AtoZ) app\n"<<endl;int v;
    cout<<"how many names do you want to sort :\n";cin >>v;
@@ -75,8 +16,7 @@ cout<<"please enter the names :\n";
    for (int i=0; i<n; i++){
     cin>>arr[i];
    }
-
-
+//selection sort algorithm
      for (int i=0; i<n; i++){
 
      for(int j=0;j<n;j++){
